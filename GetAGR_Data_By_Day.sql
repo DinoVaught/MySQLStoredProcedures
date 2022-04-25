15e0f880313226051e183cb640c69de983447c72
DinoVaught <49774308+DinoVaught@users.noreply.github.com>
2022-04-25 10:10:56 -0500

    Update GetAGR_Data_By_Day.sql
    Included Start_STN_1- Start_STN_6 in SELECT
    1) The Gage is offline when the record is inserted, the PLC is unreachable, each (start count field) defaults to 0
    2) Then, the Gage is powered on, the PLC is online and the record is flagged (complete).  Each (end count field) receives real and accurate (end count)
    3) When the math is applied (end count) - (start count = 0) the result is inaccurate, a highly inflated value
    4) When this happens the record is considered (incomplete), but the record is still needed in the external app so. . .
          I'm including the (start count) fields Start_STN_1, Start_STN_2 etc in the SELECT so the external app can evaluate Start_STN_# and take the appropriate course

--- GetAGR_Data_By_Day.sql
+++ GetAGR_Data_By_Day.sql
@@ -2,14 +2,15 @@ DROP PROCEDURE IF EXISTS mes.GetAGR_Data_By_Day;
 CREATE PROCEDURE mes.`GetAGR_Data_By_Day`(IN qryDate varchar(20))
 BEGIN
 	
+-- C:\Users\rvaught\source\repos\Gages\Database\AGR
+-- CALL debug_msg_log('GetAGR_Data_By_Day', start_date, end_date);
+
 	  DECLARE start_date VARCHAR(50);
 	  DECLARE end_date VARCHAR(50);	
 	
     SET start_date = CONCAT(SUBDATE(qryDate, INTERVAL 1 DAY), ' 22:59:00');
     SET end_date = CONCAT(qryDate, ' 23:01:00');
   
---    CALL debug_msg_log('GetAGR_Data_By_Day', start_date, end_date);
-	
 		SELECT  
 		   Start_Time,
 		   End_Time,
@@ -27,14 +28,20 @@ BEGIN
 		   (Mach_Total_End - Mach_Total_Start) + (Reject_Total_End - Reject_Total_Start) Total,
 		   (Reject_Total_End - Reject_Total_Start) AGR,
 		   (Mach_Total_End - Mach_Total_Start) Net,
-       PartNum
+		   PartNum,
+		   Start_STN_1,
+		   Start_STN_2,
+		   Start_STN_3,
+		   Start_STN_4,
+		   Start_STN_5,
+		   Start_STN_6
 		FROM agr_counts
 		WHERE End_time IS NOT NULL
 		AND RecordType = 'shift'
 		AND Start_Time BETWEEN start_date AND end_date
 		AND End_Time < end_date
-		-- AND Start_Time BETWEEN '2022-02-27 23:00:00' AND '2022-02-28 22:59:59' 
-		-- AND Gage_ID = "G25"  2022-02-27 23:00:00       2022-02-28 22:59:59
+    -- AND Start_Time BETWEEN '2022-4-20 22:59:00' AND '2022-4-21 23:01:00'
+    -- AND End_Time < '2022-4-21 23:01:00'    
 		ORDER BY Gage_ID, Start_Time;
 	END;
 
